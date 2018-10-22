1feeab3a76a9dd70b32a1b148142b3d35beacda7
gonpeche <espeche.g@gmail.com>
2018-10-22 12:14:42 -0300

    update

--- front/src/index.js
+++ front/src/index.js
@@ -1,1 +1,10 @@
-// el inicio de la app
+const React = require('react');
+const ReactDOM = require('react-dom');
+import { BrowserRouter, Route } from 'react-router'
+import Main from './containers/Main.js';
+
+ReactDOM.render(
+    <BrowserRouter>
+        <Route path="/" component={Main} />
+    </BrowserRouter>
+    , document.getElementById("app"))
